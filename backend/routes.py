9da22eb57e13c3e43e06964030177d287d20815f
Ohdele <adesinaayodele48@gmail.com>
2024-12-01 06:38:27 -0500

    Implemented GET /song endpoint

--- backend/routes.py
+++ backend/routes.py
@@ -6,7 +6,6 @@ from flask import jsonify, request, make_response, abort, url_for  # noqa; F401
 from pymongo import MongoClient
 from bson import json_util
 from pymongo.errors import OperationFailure
-from pymongo.results import InsertOneResult
 from bson.objectid import ObjectId
 import sys
 
@@ -14,8 +13,6 @@ SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 json_url = os.path.join(SITE_ROOT, "data", "songs.json")
 songs_list: list = json.load(open(json_url))
 
-# client = MongoClient(
-#     f"mongodb://{app.config['MONGO_USERNAME']}:{app.config['MONGO_PASSWORD']}@localhost")
 mongodb_service = os.environ.get('MONGODB_SERVICE')
 mongodb_username = os.environ.get('MONGODB_USERNAME')
 mongodb_password = os.environ.get('MONGODB_PASSWORD')
@@ -25,14 +22,12 @@ print(f'The value of MONGODB_SERVICE is: {mongodb_service}')
 
 if mongodb_service == None:
     app.logger.error('Missing MongoDB server in the MONGODB_SERVICE variable')
-    # abort(500, 'Missing MongoDB server in the MONGODB_SERVICE variable')
     sys.exit(1)
 
 if mongodb_username and mongodb_password:
     url = f"mongodb://{mongodb_username}:{mongodb_password}@{mongodb_service}"
 else:
     url = f"mongodb://{mongodb_service}"
-
 
 print(f"connecting to url: {url}")
 
@@ -48,6 +43,19 @@ db.songs.insert_many(songs_list)
 def parse_json(data):
     return json.loads(json_util.dumps(data))
 
-######################################################################
-# INSERT CODE HERE
-######################################################################
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({"status": "OK"}), 200
+
+@app.route('/count', methods=['GET'])
+def count():
+    """Return length of data"""
+    count = db.songs.count_documents({})
+    return jsonify({"count": count}), 200
+
+@app.route('/song', methods=['GET'])
+def songs():
+    """Fetch all songs from the database"""
+    songs = db.songs.find({})
+    song_list = list(songs)  # Convert cursor to list
+    return jsonify({"songs": song_list}), 200
